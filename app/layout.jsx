00b3ef66f7dedcdbb84451b999195416c26a1262
Pi <chakornk2007@gmail.com>
2023-07-15 19:21:18 -0700

    axios download file (doesnt work for some reason)

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body className="overflow-hidden">{children}</body>
 		</html>
 	);
 }
--- app/page.jsx
+++ app/page.jsx
@@ -3,14 +3,30 @@ import { BaseDirectory, exists, readDir } from "@tauri-apps/api/fs";
 import { appWindow } from "@tauri-apps/api/window";
 import React from "react";
 import { Settings20Regular } from "@fluentui/react-icons";
+import axios from "axios";
 
 appWindow.setDecorations(true);
+
+function getReadableFileSizeString(fileSizeInBytes) {
+	var i = -1;
+	var byteUnits = [" kB", " MB", " GB", " TB", "PB", "EB", "ZB", "YB"];
+	do {
+		fileSizeInBytes /= 1024;
+		i++;
+	} while (fileSizeInBytes > 1024);
+
+	return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
+}
 
 export default class Home extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			downloadProgess: -1,
+			downloadProgess: 50,
+			totalBytes: 0,
+			downloadedBytes: 0,
+			downloadSpeed: 0,
+			installStatus: "Downloading...",
 			currentVersion: null,
 			latestVersion: null,
 			latestRelease: {},
@@ -21,11 +37,23 @@ export default class Home extends React.Component {
 					console.log(entries);
 				}
 
-				const latestRelease = await (await fetch("https://api.github.com/repos/lemons-studios/Mission-Monkey/releases/latest")).json();
-				this.setState({ latestRelease: latestRelease });
-				this.setState({ latestVersion: latestRelease.name });
+				const res = await (await fetch("https://api.github.com/repos/lemons-studios/Mission-Monkey/releases/latest")).json();
+				console.log(res);
+				this.setState({ latestRelease: res });
+				this.setState({ latestVersion: res.name });
 			},
-			install: async () => {}
+			install: async () => {
+				console.log(this.state.latestRelease.assets[0].browser_download_url);
+				await axios.get(this.state.latestRelease.assets[0].browser_download_url, {
+					responseType: "blob",
+					onDownloadProgress: async ({ loaded, total, progress, rate }) => {
+						this.setState({ downloadedBytes: loaded });
+						this.setState({ totalBytes: total });
+						this.setState({ downloadProgess: Math.round(progress * 100) });
+						this.setState({ downloadSpeed: rate });
+					}
+				});
+			}
 		};
 	}
 	async componentDidMount() {
@@ -42,24 +70,38 @@ export default class Home extends React.Component {
 							<p>Installed: {this.state.currentVersion || "Unknown"}</p>
 						</div>
 						<div>
-							<button className="p-2 rounded-md text-[#fff] dark:text-[#000] bg-accent-light dark:bg-accent-dark hover:bg-accent-light-hover hover:dark:bg-accent-dark-hover active:bg-accent-light-active active:dark:bg-accent-dark-active">
+							<button
+								className="p-2 rounded-md text-[#fff] dark:text-[#000] bg-accent-light dark:bg-accent-dark hover:bg-accent-light-hover hover:dark:bg-accent-dark-hover active:bg-accent-light-active active:dark:bg-accent-dark-active"
+								onClick={() => {
+									this.state.install();
+								}}
+							>
 								{this.state.currentVersion ? "Check for updates" : "Install"}
 							</button>
 						</div>
 					</div>
 
-					<div
-						style={{
-							display: this.state.downloadProgess > -1 ? "block" : "none"
-						}}
-						className="w-full h-1 mt-3 rounded-full bg-[#8883] overflow-hidden"
-					>
+					<div>
 						<div
 							style={{
-								width: this.state.downloadProgess > -1 ? `${this.state.downloadProgess}%` : "0%"
+								display: this.state.downloadProgess > -1 ? "block" : "none"
 							}}
-							className="h-full bg-accent-light dark:bg-accent-dark rounded-full"
-						></div>
+							className="w-full h-1 mt-3 rounded-full bg-[#8883] overflow-hidden"
+						>
+							<div
+								style={{
+									width: this.state.downloadProgess > -1 ? `${this.state.downloadProgess}%` : "0%"
+								}}
+								className="h-full bg-accent-light dark:bg-accent-dark rounded-full"
+							></div>
+						</div>
+						<div className="flex justify-between text-xs mt-1">
+							<p>{this.state.installStatus}</p>
+							<p>
+								{this.state.downloadProgess}% ({getReadableFileSizeString(this.state.downloadedBytes)}/{getReadableFileSizeString(this.state.totalBytes)},{" "}
+								{getReadableFileSizeString(this.state.downloadSpeed)}/s)
+							</p>
+						</div>
 					</div>
 				</div>
 
