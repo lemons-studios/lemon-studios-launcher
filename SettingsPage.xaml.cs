9de327182e172523bdbf48e49930a8ee16c50728
ChakornK <chakornk2007@gmail.com>
2023-11-01 16:37:21 -0700

    clean up imports

--- SettingsPage.xaml.cs
+++ SettingsPage.xaml.cs
@@ -1,20 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System.Reflection;
-using WinUIEx;
-using Microsoft.UI.System;
 using Windows.ApplicationModel.DataTransfer;
 using System.Threading.Tasks;
 
