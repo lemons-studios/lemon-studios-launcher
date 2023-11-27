e615daea1f1e535490e7c8627fe62d74b9814bd3
ChakornK <chakornk2007@gmail.com>
2023-11-26 17:18:23 -0800

    add version dropdown on home page

--- HomePage.xaml.cs
+++ HomePage.xaml.cs
@@ -12,11 +12,52 @@ namespace Launcher
     /// </summary>
     public sealed partial class HomePage : Page
     {
+        // Initialize storage
+        private Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
         public HomePage()
         {
             this.InitializeComponent();
 
             LaunchBar.Translation += new Vector3(0, 0, 1);
+
+            LoadVersions();
+        }
+
+        private void LoadVersions()
+        {
+            // Load versions
+            string version;
+            if (localSettings.Values["selectedLaunchVer"] != null)
+            {
+                version = localSettings.Values["selectedLaunchVer"].ToString() ?? "release";
+            }
+            else
+            {
+                version = "release";
+            }
+            switch (version)
+            {
+                case "release":
+                    VersionSelect.Content = "Release";
+                    break;
+                case "beta":
+                    VersionSelect.Content = "Beta";
+                    break;
+            }
+            BetaVersionText.IsEnabled = false;
+        }
+
+        private void ReleaseVersion_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            VersionSelect.Content = "Release";
+            localSettings.Values["selectedLaunchVer"] = "release";
+        }
+
+        private void BetaVersion_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            VersionSelect.Content = "Beta";
+            localSettings.Values["selectedLaunchVer"] = "beta";
         }
     }
 }
