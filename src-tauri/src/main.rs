d13c4244628c9aa452d08f6e7f6eb850ee5379f4
ChakornK <chakornk2007@gmail.com>
2024-02-23 13:57:59 -0800

    use window_shadows crate instead

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,38 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use windows::Win32::{
-    Foundation::HWND,
-};
-use tauri::Manager;
-
-fn enable_window_shadow(window: &impl raw_window_handle::HasRawWindowHandle) {
-    use windows::Win32::{Graphics::Dwm::DwmExtendFrameIntoClientArea, UI::Controls::MARGINS};
-
-    match window.raw_window_handle() {
-        #[cfg(target_os = "windows")]
-        raw_window_handle::RawWindowHandle::Win32(handle) => {
-            let m = 1;
-            let margins = MARGINS {
-                cxLeftWidth: m,
-                cxRightWidth: m,
-                cyTopHeight: m,
-                cyBottomHeight: m,
-            };
-            unsafe {
-                let _ = DwmExtendFrameIntoClientArea(HWND(handle.hwnd as _), &margins);
-            };
-        }
-        _ => (),
-    }
-}
+use tauri::{Manager, WindowEvent};
+use window_shadows::set_shadow;
 
 fn main() {
     tauri::Builder::default()
         .setup(move |app| {
-            let window = app.get_window("main").unwrap();
-            enable_window_shadow(&window);
+            let Some(window) = app.get_window("main") else {
+                return Ok(())
+            };
+
+            set_shadow(&window, true).unwrap();
+
             Ok(())
+        })
+        .on_window_event(|e| {
+            if let WindowEvent::Resized(_) = e.event() {
+                std::thread::sleep(std::time::Duration::from_millis(1))
+            }
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
