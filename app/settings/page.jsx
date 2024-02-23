f8a26867f2b8cbab1f9b4c64a7e6c87b46ba8f26
ChakornK <chakornk2007@gmail.com>
2024-02-23 14:35:59 -0800

    add line separator between settings items

--- app/settings/page.jsx
+++ app/settings/page.jsx
@@ -4,30 +4,32 @@ import { Switch } from "../components/uicomponents";
 export default function Settings() {
 	return (
 		<div className="flex flex-col w-full h-full gap-4">
-			{[
-				{
-					name: "Recieve beta updates",
-					description: "Beta versions may be unstable",
-					type: "toggle"
-				},
-				{
-					name: "Keep launcher open",
-					description: "Keep launcher open after launching the game",
-					type: "toggle"
-				}
-			].map((e, i) => (
-				<div
-					key={i}
-					className="flex justify-between"
-				>
-					<div>
-						<p className="font-medium">{e.name}</p>
-						<p className="text-sm">{e.description}</p>
-					</div>
+			<div>
+				{[
+					{
+						name: "Recieve beta updates",
+						description: "Beta versions may be unstable",
+						type: "toggle"
+					},
+					{
+						name: "Keep launcher open",
+						description: "Keep launcher open after launching the game",
+						type: "toggle"
+					}
+				].map((e, i) => (
+					<div
+						key={i}
+						className={(i > 0 ? "border-t border-[#fff2] mt-4 pt-4 " : "") + "flex justify-between items-center"}
+					>
+						<div>
+							<p className="font-medium">{e.name}</p>
+							<p className="text-sm">{e.description}</p>
+						</div>
 
-					<Switch />
-				</div>
-			))}
+						<Switch />
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
