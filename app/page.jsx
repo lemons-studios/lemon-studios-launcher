d242c3aab993f2b6ca1ca44f4c65e0fd1c9a3937
ChakornK <chakornk2007@gmail.com>
2024-02-22 22:48:07 -0800

    collapsible sidebar

--- app/page.jsx
+++ app/page.jsx
@@ -2,10 +2,20 @@ import Sidebar from "./components/sidebar";
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-stretch p-0 min-h-screen">
+    <main className="flex flex-row justify-stretch p-0 min-h-screen">
       <Sidebar />
 
-      <div>test</div>
+      <div className="flex flex-col items-center justify-center p-8 grow">
+        <img
+          src="/lemon-studios.svg"
+          width={128}
+          style={{
+            mixBlendMode: "luminosity",
+            filter: "contrast(0.6) brightness(0.3)",
+          }}
+        />
+        <p className="opacity-30 font-medium text-2xl">Lemon Studios</p>
+      </div>
     </main>
   );
 }
