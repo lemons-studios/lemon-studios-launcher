8609844c4ec56fd415ce3bfee25f9cfafd8e8945
ChakornK <chakornk2007@gmail.com>
2024-02-23 11:39:59 -0800

    custom titlebar

---
+++ app/components/titlebar.jsx
@@ -0,0 +1,55 @@
+"use client";
+import {
+  Dismiss16Regular,
+  LineHorizontal116Regular,
+  Maximize16Regular,
+  SquareMultiple16Regular,
+} from "@fluentui/react-icons";
+import { appWindow } from "@tauri-apps/api/window";
+import { useCallback, useEffect, useState } from "react";
+
+export default function TitleBar() {
+  const [maximized, setMaximized] = useState(null);
+
+  const updateMaximized = useCallback(async () => {
+    const res = await appWindow.isMaximized();
+    setMaximized(res);
+  }, []);
+
+  useEffect(() => {
+    updateMaximized();
+    let unlisten = undefined;
+    const listen = async () => {
+      unlisten = await appWindow.onResized(() => {
+        updateMaximized();
+      });
+    };
+    listen();
+    return () => unlisten && unlisten();
+  }, [updateMaximized]);
+
+  return (
+    <div className="relative flex w-full h-14" data-tauri-drag-region>
+      <div className="right-0 absolute flex gap-1">
+        <button
+          className="flex justify-center items-center hover:bg-[#fff2] active:bg-[#fff3] w-12 h-14"
+          onClick={() => appWindow.minimize()}
+        >
+          <LineHorizontal116Regular />
+        </button>
+        <button
+          className="flex justify-center items-center hover:bg-[#fff2] active:bg-[#fff3] w-12 h-14"
+          onClick={() => appWindow.toggleMaximize()}
+        >
+          {maximized ? <SquareMultiple16Regular /> : <Maximize16Regular />}
+        </button>
+        <button
+          className="flex justify-center items-center hover:bg-[#ca3230] active:bg-[#84292c] w-12 h-14"
+          onClick={() => appWindow.close()}
+        >
+          <Dismiss16Regular />
+        </button>
+      </div>
+    </div>
+  );
+}
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
-"use client";
 import Sidebar from "./components/sidebar";
+import TitleBar from "./components/titlebar";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
@@ -9,7 +9,12 @@ export default function RootLayout({ children }) {
         <main className="flex flex-row justify-stretch p-0 min-h-screen">
           <Sidebar />
 
-          <div className="p-8 grow">{children}</div>
+          <div className="flex flex-col justify-stretch bg-[#050505] p-0 grow">
+            <TitleBar />
+            <div className="bg-[#151515] p-8 rounded-tl-2xl grow">
+              {children}
+            </div>
+          </div>
         </main>
       </body>
     </html>
