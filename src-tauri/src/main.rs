b6bff36cbf2e0d1686dd2b3d351095641e57caae
ChakornK <chakornk2007@gmail.com>
2024-02-23 12:01:43 -0800

    enable window shadows in Windows

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,41 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use windows::Win32::{
+    Foundation::HWND,
+    UI::WindowsAndMessaging::{AnimateWindow, AW_BLEND, AW_HIDE},
+};
+use raw_window_handle::HasRawWindowHandle;
+use tauri::Manager;
+
+fn enable_window_shadow(window: &impl raw_window_handle::HasRawWindowHandle) {
+    use windows::Win32::{Graphics::Dwm::DwmExtendFrameIntoClientArea, UI::Controls::MARGINS};
+
+    match window.raw_window_handle() {
+        #[cfg(target_os = "windows")]
+        raw_window_handle::RawWindowHandle::Win32(handle) => {
+            let m = 1;
+            let margins = MARGINS {
+                cxLeftWidth: m,
+                cxRightWidth: m,
+                cyTopHeight: m,
+                cyBottomHeight: m,
+            };
+            unsafe {
+                let _ = DwmExtendFrameIntoClientArea(HWND(handle.hwnd as _), &margins);
+            };
+        }
+        _ => (),
+    }
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(move |app| {
+            let window = app.get_window("main").unwrap();
+            enable_window_shadow(&window);
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
