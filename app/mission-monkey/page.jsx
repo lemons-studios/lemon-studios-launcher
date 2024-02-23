4db471738d4c602e308570085330b7f212afd7c2
ChakornK <chakornk2007@gmail.com>
2024-02-23 11:38:42 -0800

    mission monkey page

--- app/mission-monkey/page.jsx
+++ app/mission-monkey/page.jsx
@@ -2,16 +2,12 @@ import Sidebar from "../components/sidebar";
 
 export default function MissionMonkey() {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full">
-      <img
-        src="/mission-monkey.png"
-        width={128}
-        style={{
-          mixBlendMode: "luminosity",
-          filter: "contrast(0.6) brightness(0.3)",
-        }}
-      />
-      <p className="opacity-30 font-medium text-2xl">Mission: Monkey</p>
+    <div className="flex flex-col items-center justify-center gap-4 w-full h-full">
+      <img src="/mission-monkey.png" width={128} />
+      <p className="font-medium text-4xl potra">Mission: Monkey</p>
+      <button className="flex flex-row items-center justify-center bg-green-500 hover:bg-green-600 pt-1 w-28 h-10 font-bold text-white transition-colors rounded">
+        Play
+      </button>
     </div>
   );
 }
