13292d45cdf773e3eef6dbfe6fbea2f9c953989a
ChakornK <chakornk2007@gmail.com>
2024-02-22 23:16:07 -0800

    fixed bugs

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,17 @@
+"use client";
+import Sidebar from "./components/sidebar";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="overflow-hidden">{children}</body>
+      <body className="overflow-hidden">
+        <main className="flex flex-row justify-stretch p-0 min-h-screen">
+          <Sidebar />
+
+          <div className="p-8 grow">{children}</div>
+        </main>
+      </body>
     </html>
   );
 }
--- app/settings/page.jsx
+++ app/settings/page.jsx
@@ -2,20 +2,16 @@ import Sidebar from "../components/sidebar";
 
 export default function Settings() {
   return (
-    <main className="flex flex-row justify-stretch p-0 min-h-screen">
-      <Sidebar />
-
-      <div className="flex flex-col items-center justify-center p-8 grow">
-        <img
-          src="/lemon-studios.svg"
-          width={128}
-          style={{
-            mixBlendMode: "luminosity",
-            filter: "contrast(0.6) brightness(0.3)",
-          }}
-        />
-        <p className="opacity-30 font-medium text-2xl">Lemon Studios</p>
-      </div>
-    </main>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <img
+        src="/lemon-studios.svg"
+        width={128}
+        style={{
+          mixBlendMode: "luminosity",
+          filter: "contrast(0.6) brightness(0.3)",
+        }}
+      />
+      <p className="opacity-30 font-medium text-2xl">Settings</p>
+    </div>
   );
 }
