0cd217eb014716548a63cd98363a580826582669
ChakornK <chakornk2007@gmail.com>
2024-02-27 08:15:25 -0800

    ✨ feat(home page): fetch news article from CDN and display them

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,46 @@
+"use client";
+import { useEffect, useState } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 export default function Home() {
 	return (
-		<div className="flex flex-col justify-center items-center w-full h-full slide-in">
-			<img
-				src="/lemon-studios.svg"
-				width={128}
-				style={{
-					mixBlendMode: "luminosity",
-					filter: "contrast(0.6) brightness(0.3)",
-				}}
-			/>
-			<p className="opacity-30 font-medium text-2xl">Lemon Studios</p>
+		<div className="flex flex-col w-full h-full slide-in">
+			<h1 className="mb-4 font-normal text-2xl">Latest news</h1>
+			<NewsGrid />
 		</div>
+	);
+}
+
+function NewsGrid() {
+	const [news, setNews] = useState([]);
+
+	useEffect(() => {
+		fetch("https://cdn.lemon-studios.ca/lemon-launcher-feed/index.json")
+			.then((r) => r.json())
+			.then((j) => setNews(j));
+	}, []);
+
+	return (
+		<SkeletonTheme baseColor="#222" highlightColor="#666" borderRadius={100}>
+			<div className="flex flex-wrap gap-2">
+				{(news.length > 0 ? news : Array.from(Array(3), (_e, i) => i)).map(
+					(e, i) => (
+						<div
+							className="border-[#222] hover:bg-[#222] p-4 border rounded-lg w-full md:w-[calc(100%/2-0.5rem)] lg:w-[calc(100%/3-1rem)] h-32 cursor-pointer "
+							key={i}
+							style={{
+								pointerEvents: news.length > 0 ? "all" : "none",
+							}}
+						>
+							<p className="mb-2 text-xl">{e.name || <Skeleton />}</p>
+							<p className="text-[#888] text-sm">
+								{e.description || <Skeleton count={2.6} />}
+							</p>
+						</div>
+					)
+				)}
+			</div>
+		</SkeletonTheme>
 	);
 }
